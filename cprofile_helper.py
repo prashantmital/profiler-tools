d637e7cbf7bc561402c6f9cafcd22bc61588534b
Prashant Mital <prashantmital@gmail.com>
2016-10-30 10:55:39 -0500

    Add cprofile decorator

---
+++ cprofile_helper.py
@@ -0,0 +1,23 @@
+import cProfile
+import datetime
+
+
+def profile_performance(write_profile=False,
+                        profile_name="{funcname}-{timestamp}"):
+    def profiled_func_decorator(func):
+        def profiled_func(*args, **kwargs):
+            profile = cProfile.Profile()
+            try:
+                profile.enable()
+                result = func(*args, **kwargs)
+                profile.disable()
+            finally:
+                if write_profile:
+                    profile.dump_stats("{}.prof".format(profile_name.format(
+                        funcname=func.__name__,
+                        timestamp=str(datetime.datetime.now()).replace(' ', '_')
+                    )))
+                profile.print_stats()
+                return result
+        return profiled_func
+    return profiled_func_decorator
