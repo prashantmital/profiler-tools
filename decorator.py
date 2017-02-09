f86416700935f73e134cba68066128430f03a58c
Prashant Mital <prashantmital@gmail.com>
2017-02-09 14:17:03 -0600

    ENH: better exception handling in profiler

--- decorator.py
+++ decorator.py
@@ -2,7 +2,8 @@ import cProfile
 import datetime
 
 
-def profile(write_profile=False, profile_name="{funcname}-{timestamp}"):
+def profile(write_profile=False, profile_name="{funcname}-{timestamp}",
+            catch_exception=Exception):
     def profiled_func_decorator(func):
         def profiled_func(*args, **kwargs):
             profile = cProfile.Profile()
@@ -10,6 +11,8 @@ def profile(write_profile=False, profile_name="{funcname}-{timestamp}"):
                 profile.enable()
                 result = func(*args, **kwargs)
                 profile.disable()
+            except catch_exception as exc:
+                result = exc
             finally:
                 if write_profile:
                     profile.dump_stats("{}.prof".format(profile_name.format(
@@ -17,6 +20,9 @@ def profile(write_profile=False, profile_name="{funcname}-{timestamp}"):
                         timestamp=str(datetime.datetime.now()).replace(' ', '_')
                     )))
                 profile.print_stats()
-                return result
+                if isinstance(result, catch_exception):
+                    raise result
+                else:
+                    return result
         return profiled_func
     return profiled_func_decorator
